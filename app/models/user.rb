d2ab42d27c2fe3dab04917cccd66f73363a4d029
izumiyama-daisuke <74485639+izumiyama-daisuke@users.noreply.github.com>
2021-01-22 04:20:46 +0900

    ログイン機能の実装

--- app/models/user.rb
+++ app/models/user.rb
@@ -1,4 +1,14 @@
 class User < ApplicationRecord
+  validates :nickname, presence: true
+  with_options presence: true, format: { with: /\A[ぁ-んァ-ヶ一-龥々]+\z/, message: 'に全角文字を使用してください' } do
+    validates :first_name, presence: true
+    validates :last_name, presence: true
+  end
+  with_options presence: true, format: { with: /\A[ァ-ヶ一]+\z/, message: 'に全角文字カタカナを使用してください' } do
+    validates :first_kana, presence: true
+    validates :last_kana, presence: true
+  end
+  validates :birthday, presence: true
   # Include default devise modules. Others available are:
   # :confirmable, :lockable, :timeoutable, :trackable and :omniauthable
   devise :database_authenticatable, :registerable,
