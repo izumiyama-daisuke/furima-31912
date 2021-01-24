f8945d1e71878568d88264dd531b6ee091256449
izumiyama-daisuke <74485639+izumiyama-daisuke@users.noreply.github.com>
2021-01-24 19:50:50 +0900

    不必要なコメントアウトを削除

--- app/models/user.rb
+++ app/models/user.rb
@@ -4,7 +4,10 @@ class User < ApplicationRecord
   PASSWORD_REGEX = /\A(?=.*?[a-z])(?=.*?\d)[a-z\d]+\z/i.freeze
   validates_format_of :password, with: PASSWORD_REGEX, message: 'には英字と数字の両方を含めて設定してください'
 
-  validates :password_confirmation, presence: true
+  with_options presence: true do
+    validates :password_confirmation, presence: true
+    validates :birthday, presence: true
+  end
 
   NAME_REGEX = /\A[ぁ-んァ-ヶ一-龥々]+\z/.freeze
   validates_format_of :first_name, presence: true, with: NAME_REGEX, message: '全角（漢字・ひらがな・カタカナ）を使用してください'
@@ -14,10 +17,6 @@ class User < ApplicationRecord
   validates_format_of :first_kana, presence: true, with: NAME_KANA_REGEX, message: 'に全角（カタカナ）を使用してください'
   validates_format_of :last_kana, presence: true, with: NAME_KANA_REGEX, message: 'に全角（カタカナ）を使用してください'
 
-  validates :birthday, presence: true
-
-  # Include default devise modules. Others available are:
-  # :confirmable, :lockable, :timeoutable, :trackable and :omniauthable
   devise :database_authenticatable, :registerable,
          :recoverable, :rememberable, :validatable
 end
--- config/routes.rb
+++ config/routes.rb
@@ -3,18 +3,3 @@ Rails.application.routes.draw do
   
   root to: "items#index"
 end
-
-
-#Rails.application.routes.draw do
-  
-#  devise_for :users
-#  get 'prototypes/index'
-#  root to: "prototypes#index"
-
-#  resources :users, only: :show
-#  resources :prototypes do
-    #resources :comments, only: :create
-#    resources :comments, only: [:index, :create]
-#  end
-  
-#end
