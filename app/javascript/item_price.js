34a53166a1a5abd9bd6ba438f9dc5176de03d260
izumiyama-daisuke <74485639+izumiyama-daisuke@users.noreply.github.com>
2021-02-02 22:58:48 +0900

    不必要なコードを駆除

--- app/javascript/item_price.js
+++ app/javascript/item_price.js
@@ -1,18 +1,10 @@
 window.addEventListener('load', () =>{
-  console.log("ok");
-
   const priceInput = document.getElementById("item-price");
-  console.log(priceInput);
   priceInput.addEventListener("input", () => {
-    //console.log("イベント発火");
     const inputValue = priceInput.value;
-    //console.log(inputValue);
     const addTaxDom = document.getElementById("add-tax-price")
     addTaxDom.innerHTML = Math.floor(inputValue * 0.1)
-    //console.log(addTaxDom);
     const priceProfit = document.getElementById("profit")
-    console.log(priceProfit);
     priceProfit.innerHTML = Math.floor(inputValue - (inputValue * 0.1));
   })
-
 });
