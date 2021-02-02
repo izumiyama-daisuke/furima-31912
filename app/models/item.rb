a8513aa68312271a288b8bb808700f78063df55c
izumiyama-daisuke <74485639+izumiyama-daisuke@users.noreply.github.com>
2021-02-02 13:36:38 +0900

    不要なコードの削除

--- app/models/item.rb
+++ app/models/item.rb
@@ -10,7 +10,7 @@ class Item < ApplicationRecord
   belongs_to :prefecture
   belongs_to :day
 
-  # 空の投稿を保存できないようにする
+  
   with_options presence: true do
     validates :item_name
     validates :description
@@ -19,7 +19,7 @@ class Item < ApplicationRecord
 
   validates :price, presence: true, inclusion: { in: 300..9_999_999 }
 
-  # ジャンルの選択が「--」の時は保存できないようにする
+  
   validates :category_id, numericality: { other_than: 1 }
   validates :state_id, numericality: { other_than: 1 }
   validates :delivery_fee_id, numericality: { other_than: 1 }
