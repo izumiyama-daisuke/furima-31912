4dc7123f4e6a41fab9c8129974428964cb9b98c7
izumiyama-daisuke <74485639+izumiyama-daisuke@users.noreply.github.com>
2021-02-03 19:56:44 +0900

    コードレヴュー後の修正２

--- app/models/item.rb
+++ app/models/item.rb
@@ -17,14 +17,6 @@ class Item < ApplicationRecord
     validates :price, inclusion: { in: 300..9_999_999 }
   end
 
-  # validates :price, presence: true, inclusion: { in: 300..9_999_999 }
-
-  # validates :category_id, numericality: { other_than: 1 }
-  # validates :state_id, numericality: { other_than: 1 }
-  # validates :delivery_fee_id, numericality: { other_than: 1 }
-  # validates :prefecture_id, numericality: { other_than: 1 }
-  # validates :day_id, numericality: { other_than: 1 }
-
   with_options numericality: { other_than: 1 } do
     validates :category_id
     validates :state_id
