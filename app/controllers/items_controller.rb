85f17b4ce9af95689b59653eafa367e8eab38cc7
izumiyama-daisuke <74485639+izumiyama-daisuke@users.noreply.github.com>
2021-02-04 14:06:21 +0900

    微修正

--- app/controllers/items_controller.rb
+++ app/controllers/items_controller.rb
@@ -1,8 +1,8 @@
 class ItemsController < ApplicationController
   before_action :authenticate_user!, except: [:index]
   def index
-    #お試しで記述しました
-    @item = Item.all.order(created_at: :desc) 
+    # お試しで記述しました
+    @item = Item.all.order(created_at: :desc)
   end
 
   def new
@@ -21,6 +21,7 @@ class ItemsController < ApplicationController
   private
 
   def item_params
-    params.require(:item).permit(:image, :item_name, :description, :price, :category_id, :state_id, :delivery_fee_id, :prefecture_id, :day_id).merge(user_id: current_user.id)
+    params.require(:item).permit(:image, :item_name, :description, :price, :category_id, :state_id, :delivery_fee_id,
+                                 :prefecture_id, :day_id).merge(user_id: current_user.id)
   end
 end
