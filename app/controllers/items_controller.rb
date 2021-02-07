337184dee821bc5b3a79cc1a956adb0895063507
izumiyama-daisuke <74485639+izumiyama-daisuke@users.noreply.github.com>
2021-02-07 12:13:39 +0900

    edirect_to先をprefix型で記述

--- app/controllers/items_controller.rb
+++ app/controllers/items_controller.rb
@@ -34,7 +34,7 @@ class ItemsController < ApplicationController
     @item = Item.find(params[:id])
     @item.update(item_params)
     if @item.save
-      redirect_to "/items/#{@item.id}"
+      redirect_to item_path(@item.id)
     else
       render 'edit'
     end
