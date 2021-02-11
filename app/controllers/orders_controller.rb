f47e8df850bb72ce578b5836f6053627f44b7f3e
izumiyama-daisuke <74485639+izumiyama-daisuke@users.noreply.github.com>
2021-02-11 11:03:19 +0900

    oederモデルへ登録

--- app/controllers/orders_controller.rb
+++ app/controllers/orders_controller.rb
@@ -4,5 +4,28 @@ class OrdersController < ApplicationController
   end
 
   def create
+    @order = Order.new(order_params)
+    if @order.save
+      redirect_to action: :index
+    else
+      render action: :index
+    end
+  end
+  #def create
+    #@user = User.new(donation_params)
+    #if @user.save
+      #redirect_to action: :index
+    #else
+      #render action: :new
+    #end
+  #end
+
+  private
+
+  def order_params
+    #params.require(:order).permit(:item_id).merge(user_id: current_user.id)
+    #params.require(:order).permit().merge(user_id: current_user.id, item_id: :item_id)
+    #params.require(:order).merge(user_id: current_user.id, item_id: :item_id)
+    params.permit(:item_id).merge(user_id: current_user.id)
   end
 end
