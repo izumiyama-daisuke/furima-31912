98961d3bcc5b8473d5aa6a8f060d6005c737c4ed
izumiyama-daisuke <74485639+izumiyama-daisuke@users.noreply.github.com>
2021-02-14 21:42:57 +0900

    ログイン状態の出品者が、URLを直接入力して自身の出品した商品購入ページに遷移しようとすると、トップページに遷移する

--- app/controllers/orders_controller.rb
+++ app/controllers/orders_controller.rb
@@ -1,6 +1,9 @@
 class OrdersController < ApplicationController
   def index
     @item = Item.find(params[:item_id])
+    if @item.user == current_user
+      redirect_to root_path
+    end
   end
 
   def create
