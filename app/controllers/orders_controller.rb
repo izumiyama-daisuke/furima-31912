5b2a2d2116f59a90af81ebf75bc2bd845b7ea596
izumiyama-daisuke <74485639+izumiyama-daisuke@users.noreply.github.com>
2021-02-15 16:04:24 +0900

    エラーメッセージを表示する機能を微修正

--- app/controllers/orders_controller.rb
+++ app/controllers/orders_controller.rb
@@ -2,41 +2,30 @@ class OrdersController < ApplicationController
   before_action :authenticate_user!
   def index
     @item = Item.find(params[:item_id])
-    @order = Order.all
+    #@order = Order.all
+    @item_order = ItemOrder.new##ためし
     if @item.user == current_user
       redirect_to root_path
     end
   end
 
   def create
-  #  @order = Order.new(order_params)
-  #  @order.save
-  #  Address.create(address_params)
-  #  redirect_to root_path(order_params)
-
-
     @item_order = ItemOrder.new(order_params)
     if @item_order.valid?
       @item_order.save
       redirect_to root_path
     else
-      @item = Item.find(params[:item_id])##ためし
-      render action: :index
+      #@item_order = ItemOrder.new##ためし
+      @item = Item.find(params[:item_id])##購入ページの写真表示のため
       #render root_path
-      #render items/:item_id/orders(order_params)
+      render action: :index
     end
   end
 
   private
 
-#  def order_params
-#    params.permit(:item_id).merge(user_id: current_user.id)
-#  end
-
-#  def address_params
-#    params.permit(:postal_code, :prefecture_id, :city, :address1, :address2, :telephone, :item_id)
-#  end
   def order_params
-    params.permit(:postal_code, :prefecture_id, :city, :address1, :address2, :telephone, :item_id).merge(user_id: current_user.id)
+    #params.require(:item_order).permit(:postal_code, :prefecture_id, :city, :address1, :address2, :telephone, :item_id).merge(user_id: current_user.id)
+    params.require(:item_order).permit(:postal_code, :prefecture_id, :city, :address1, :address2, :telephone).merge(user_id: current_user.id, item_id: params[:item_id])
   end#require(:item_order).
 end
