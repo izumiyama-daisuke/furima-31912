e10dc46bac508d109a7dd01f300f673b8c2e2013
izumiyama-daisuke <74485639+izumiyama-daisuke@users.noreply.github.com>
2021-03-11 13:32:28 +0900

    購入テストコードを修正

--- app/controllers/items_controller.rb
+++ app/controllers/items_controller.rb
@@ -13,7 +13,6 @@ class ItemsController < ApplicationController
   def create
     @item = Item.new(item_params)
 
-
     if @item.save
       redirect_to root_path
     else
@@ -53,7 +52,7 @@ class ItemsController < ApplicationController
   private
 
   def item_params
-    #params.require(:item).permit(:image, :item_name, :description, :price, :category_id, :state_id, :delivery_fee_id,
+    # params.require(:item).permit(:image, :item_name, :description, :price, :category_id, :state_id, :delivery_fee_id,
     #                             :prefecture_id, :day_id).merge(user_id: current_user.id)
     params.require(:item).permit(:item_name, :description, :price, :category_id, :state_id, :delivery_fee_id,
                                  :prefecture_id, :day_id, images: []).merge(user_id: current_user.id)
--- spec/factories/items.rb
+++ spec/factories/items.rb
@@ -9,11 +9,10 @@ FactoryBot.define do
     day_id                  { '2' }
     price                   { '300' }
 
-
     association :user
 
     after(:build) do |item|
-      item.image.attach(io: File.open('public/images/test_image.png'), filename: 'test_image.png')
+      item.images.attach(io: File.open('public/images/test_image.png'), filename: 'test_image.png')
     end
   end
 end
